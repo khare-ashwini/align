66b7ae827bff599e4c02f8e5ce5a75bef658b45d
chandra <mr.cpkhatri@gmail.com>
2014-10-09 22:47:41 -0400

    Parameters Button double click fixed

--- OpenTripPlanner/AlignParameterViewController.h
+++ OpenTripPlanner/AlignParameterViewController.h
@@ -15,6 +15,7 @@
 @property (weak, nonatomic) IBOutlet UIView *allParameterView;
 @property (weak, nonatomic) IBOutlet UIView *myParameterView;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
+
 - (IBAction)parameterSegment:(id)sender;
 - (IBAction)traffic:(id)sender;
 - (IBAction)greenery:(id)sender;
@@ -28,5 +29,16 @@
 - (IBAction)landVariables:(id)sender;
 
 - (IBAction)done:(id)sender;
+
+@property (weak, nonatomic) IBOutlet UIButton *trafficButton;
+@property (weak, nonatomic) IBOutlet UIButton *greeneryButton;
+@property (weak, nonatomic) IBOutlet UIButton *crimeButton;
+@property (weak, nonatomic) IBOutlet UIButton *sidewalkButton;
+@property (weak, nonatomic) IBOutlet UIButton *slopeButton;
+@property (weak, nonatomic) IBOutlet UIButton *residentialButton;
+@property (weak, nonatomic) IBOutlet UIButton *businessButton;
+@property (weak, nonatomic) IBOutlet UIButton *accessibilityButton;
+@property (weak, nonatomic) IBOutlet UIButton *intersectionsButton;
+@property (weak, nonatomic) IBOutlet UIButton *landvariationsButton;
 
 @end
--- OpenTripPlanner/AlignParameterViewController.m
+++ OpenTripPlanner/AlignParameterViewController.m
@@ -28,17 +28,7 @@
 
 @implementation AlignParameterViewController
 @synthesize allParameterView, myParameterView;
-
-NSInteger temp_traffic = 0;
-NSInteger temp_slope = 0;
-NSInteger temp_crime = 0;
-NSInteger temp_accessibility = 0;
-NSInteger temp_residential = 0;
-NSInteger temp_business = 0;
-NSInteger temp_greenery = 0;
-NSInteger temp_sidewalk = 0;
-NSInteger temp_intersections = 0;
-NSInteger temp_landvariations = 0;
+@synthesize trafficButton, greeneryButton, slopeButton, crimeButton, accessibilityButton, residentialButton, sidewalkButton, intersectionsButton, landvariationsButton;
 
 NSInteger traffic = 0;
 NSInteger slope = 0;
@@ -118,15 +108,7 @@ NSInteger landvariations = 0;
 }
 
 - (IBAction)traffic:(id)sender {
-    if (temp_traffic ==1){
-        
-        [sender setTitle:@"Traffic E!" forState:UIControlStateNormal];
-        temp_traffic = 0;
-    }else if (temp_traffic==2){
-        
-        [sender setTitle:@"Traffic S!" forState:UIControlStateNormal];
-        temp_traffic = 0;
-    }
+    
     UIAlertView *trafficPopup = [[UIAlertView alloc] initWithTitle:@"Traffic!" message:@"" delegate:self cancelButtonTitle:@"No Preference" otherButtonTitles:@"Introduction",@"Set as essential", @"Set as secondary", nil];
     
     trafficPopup.tag = 1;
@@ -137,15 +119,6 @@ NSInteger landvariations = 0;
 
 - (IBAction)greenery:(id)sender {
     
-    if (temp_greenery ==1){
-        [sender setTitle:@"Greenery E!" forState:UIControlStateNormal];
-        temp_greenery = 0;
-        
-    }else if (temp_greenery == 2){
-        [sender setTitle:@"Greenery S!" forState:UIControlStateNormal];
-        temp_greenery = 0;
-        
-    }
     UIAlertView *greeneryPopup = [[UIAlertView alloc] initWithTitle:@"Greenery!" message:@"" delegate:self cancelButtonTitle:@"No Preference" otherButtonTitles:@"Introduction",@"Set as essential", @"Set as secondary", nil];
     
     greeneryPopup.tag = 2;
@@ -155,13 +128,6 @@ NSInteger landvariations = 0;
 
 - (IBAction)crime:(id)sender {
     
-    if (temp_crime ==1){
-        [sender setTitle:@"Crime E!" forState:UIControlStateNormal];
-        temp_crime = 0;
-    }else if (temp_crime ==2){
-        [sender setTitle:@"Crime S!" forState:UIControlStateNormal];
-        temp_crime = 0;
-    }
     UIAlertView *crimePopup = [[UIAlertView alloc] initWithTitle:@"Crime!" message:@"" delegate:self cancelButtonTitle:@"No Preference" otherButtonTitles:@"Introduction",@"Set as essential", @"Set as secondary", nil];
     
     crimePopup.tag = 3;
@@ -170,13 +136,7 @@ NSInteger landvariations = 0;
 
 
 - (IBAction)sidewalk:(id)sender {
-    if (temp_sidewalk ==1){
-        [sender setTitle:@"Sidewalk E!" forState:UIControlStateNormal];
-        temp_sidewalk = 0;
-    }else if (temp_sidewalk==2){
-        [sender setTitle:@"Sidewalk S!" forState:UIControlStateNormal];
-        temp_sidewalk = 0;
-    }
+    
     UIAlertView *sidewalkPopup = [[UIAlertView alloc] initWithTitle:@"Sidewalk!" message:@"" delegate:self cancelButtonTitle:@"No Preference" otherButtonTitles:@"Introduction",@"Set as essential", @"Set as secondary", nil];
     
     sidewalkPopup.tag = 4;
@@ -185,13 +145,6 @@ NSInteger landvariations = 0;
 
 - (IBAction)Slope:(id)sender {
     
-    if (temp_slope ==1){
-        [sender setTitle:@"Slope E!" forState:UIControlStateNormal];
-        temp_slope = 0;
-    }else if (temp_slope ==2){
-        [sender setTitle:@"Slope S!" forState:UIControlStateNormal];
-        temp_slope = 0;
-    }
     UIAlertView *slopePopup = [[UIAlertView alloc] initWithTitle:@"Slope!" message:@"" delegate:self cancelButtonTitle:@"No Preference" otherButtonTitles:@"Introduction",@"Set as essential", @"Set as secondary", nil];
     
     slopePopup.tag = 5;
@@ -200,13 +153,7 @@ NSInteger landvariations = 0;
 
 
 - (IBAction)residentialDensity:(id)sender {
-    if (temp_residential ==1){
-        [sender setTitle:@"ResidentialDensity E!" forState:UIControlStateNormal];
-        temp_residential = 0;
-    }else if (temp_residential ==2){
-        [sender setTitle:@"ResidentialDensity S!" forState:UIControlStateNormal];
-        temp_residential = 0;
-    }
+    
     UIAlertView *residentialPopup = [[UIAlertView alloc] initWithTitle:@"Residential Density!" message:@"" delegate:self cancelButtonTitle:@"No Preference" otherButtonTitles:@"Introduction",@"Set as essential", @"Set as secondary", nil];
     
     residentialPopup.tag = 6;
@@ -215,13 +162,7 @@ NSInteger landvariations = 0;
 
 
 - (IBAction)businessDensity:(id)sender {
-    if (temp_business ==1){
-        [sender setTitle:@"BusinessDensity E!" forState:UIControlStateNormal];
-        temp_business = 0;
-    }else if (temp_business ==2){
-        [sender setTitle:@"BusinessDensity S!" forState:UIControlStateNormal];
-        temp_business = 0;
-    }
+    
     UIAlertView *businessPopup = [[UIAlertView alloc] initWithTitle:@"Business Density!" message:@"" delegate:self cancelButtonTitle:@"No Preference" otherButtonTitles:@"Introduction",@"Set as essential", @"Set as secondary", nil];
     
     businessPopup.tag =7;
@@ -231,13 +172,7 @@ NSInteger landvariations = 0;
 
 - (IBAction)accessibility:(id)sender {
     
-    if (temp_accessibility ==1){
-        [sender setTitle:@"Accessibility E!" forState:UIControlStateNormal];
-        temp_accessibility = 0;
-    }else if (temp_accessibility ==2){
-        [sender setTitle:@"Accessibility S!" forState:UIControlStateNormal];
-        temp_accessibility = 0;
-    }
+    
     UIAlertView *accessibilityPopup = [[UIAlertView alloc] initWithTitle:@"Accessibility!" message:@"" delegate:self cancelButtonTitle:@"No Preference" otherButtonTitles:@"Introduction",@"Set as essential", @"Set as secondary", nil];
     
     accessibilityPopup.tag = 8;
@@ -248,13 +183,7 @@ NSInteger landvariations = 0;
 
 
 - (IBAction)intersections:(id)sender {
-    if (temp_intersections ==1){
-        [sender setTitle:@"Intersections E!" forState:UIControlStateNormal];
-        temp_intersections = 0;
-    }else if (temp_intersections ==2){
-        [sender setTitle:@"Intersections S!" forState:UIControlStateNormal];
-        temp_intersections = 0;
-    }
+    
     UIAlertView *intersectionsPopup = [[UIAlertView alloc] initWithTitle:@"Intersections!" message:@"" delegate:self cancelButtonTitle:@"No Preference" otherButtonTitles:@"Introduction",@"Set as essential", @"Set as secondary", nil];
     
     intersectionsPopup.tag = 9;
@@ -265,13 +194,7 @@ NSInteger landvariations = 0;
 
 
 - (IBAction)landVariables:(id)sender {
-    if (temp_landvariations ==1){
-        [sender setTitle:@"Landvariations E!" forState:UIControlStateNormal];
-        temp_landvariations = 0;
-    }else if (temp_landvariations ==2){
-        [sender setTitle:@"Landvariations S!" forState:UIControlStateNormal];
-        temp_landvariations = 0;
-    }
+    
     UIAlertView *landvariationsPopup = [[UIAlertView alloc] initWithTitle:@"Landvariations!" message:@"" delegate:self cancelButtonTitle:@"No Preference" otherButtonTitles:@"Introduction",@"Set as essential", @"Set as secondary", nil];
     
     landvariationsPopup.tag = 10;
@@ -288,12 +211,11 @@ NSInteger landvariations = 0;
             
         } else if (buttonIndex == 2){
             traffic = 5;
-            //[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://www.youtube.com"]]];
-            temp_traffic = 1;
+               [self.trafficButton setTitle:@"Traffic E!" forState:UIControlStateNormal];
         } else if (buttonIndex == 3){
             traffic = 2;
-            //[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://www.facebook.com"]]];
-            temp_traffic = 2;
+            [self.trafficButton setTitle:@"Traffic S!" forState:UIControlStateNormal];
+            
         }
     } else if (alertView.tag == 2){
         if (buttonIndex == 1){
@@ -302,12 +224,11 @@ NSInteger landvariations = 0;
             
         } else if (buttonIndex == 2){
             greenery = 5;
-            //[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://www.youtube.com"]]];
-            temp_greenery = 1;
+            [self.greeneryButton setTitle:@"Greenery E!" forState:UIControlStateNormal];
+            
         } else if (buttonIndex == 3){
             greenery = 2;
-            //[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://www.facebook.com"]]];
-            temp_greenery = 2;
+            [self.greeneryButton setTitle:@"Greenery S!" forState:UIControlStateNormal];
         }
     }else if (alertView.tag == 3){
         if (buttonIndex == 1){
@@ -316,12 +237,12 @@ NSInteger landvariations = 0;
             
         } else if (buttonIndex == 2){
             crime = 5;
-            //[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://www.youtube.com"]]];
-            temp_crime = 1;
+            [self.crimeButton setTitle:@"Crime E!" forState:UIControlStateNormal];
+            
         } else if (buttonIndex == 3){
-            traffic = 3;
-            //[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://www.facebook.com"]]];
-            temp_crime = 2;
+            crime = 2;
+            [self.crimeButton setTitle:@"Crime S!" forState:UIControlStateNormal];
+            
         }
     }else if (alertView.tag == 4){
         
@@ -331,12 +252,12 @@ NSInteger landvariations = 0;
             
         } else if (buttonIndex == 2){
             sidewalk = 5;
-            //[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://www.youtube.com"]]];
-            temp_sidewalk = 1;
+            [self.sidewalkButton setTitle:@"Sidewalk E!" forState:UIControlStateNormal];
+            
         } else if (buttonIndex == 3){
-            sidewalk = 3;
-            //[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://www.facebook.com"]]];
-            temp_sidewalk = 2;
+            sidewalk = 2;
+            [self.sidewalkButton setTitle:@"Sidewalk S!" forState:UIControlStateNormal];
+            
         }
         
     }else if (alertView.tag == 5){
@@ -347,12 +268,12 @@ NSInteger landvariations = 0;
             
         } else if (buttonIndex == 2){
             slope = 5;
-            //[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://www.youtube.com"]]];
-            temp_slope = 1;
+            [self.slopeButton setTitle:@"Slope E!" forState:UIControlStateNormal];
+            
         } else if (buttonIndex == 3){
-            traffic = 3;
-            //[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://www.facebook.com"]]];
-            temp_slope = 2;
+            slope = 2;
+            [self.slopeButton setTitle:@"Slope S!" forState:UIControlStateNormal];
+            
         }
         
     }else if (alertView.tag == 6){
@@ -363,12 +284,13 @@ NSInteger landvariations = 0;
             
         } else if (buttonIndex == 2){
             residential = 5;
-            //[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://www.youtube.com"]]];
-            temp_residential = 1;
+            [self.residentialButton setTitle:@"Residential Density E!" forState:UIControlStateNormal];
+            
+            
         } else if (buttonIndex == 3){
-            residential = 3;
-            //[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://www.facebook.com"]]];
-            temp_residential = 2;
+            residential = 2;
+            [self.residentialButton setTitle:@"Residential Density S!" forState:UIControlStateNormal];
+            
         }
         
     }else if (alertView.tag == 7){
@@ -379,12 +301,12 @@ NSInteger landvariations = 0;
             
         } else if (buttonIndex == 2){
             business = 5;
-            //[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://www.youtube.com"]]];
-            temp_business = 1;
+            [self.businessButton setTitle:@"Business Density E!" forState:UIControlStateNormal];
+            
         } else if (buttonIndex == 3){
-            business = 3;
-            //[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://www.facebook.com"]]];
-            temp_business = 2;
+            business = 2;
+            [self.businessButton setTitle:@"Business Density S!" forState:UIControlStateNormal];
+            
         }
         
     } else if (alertView.tag == 8){
@@ -395,12 +317,12 @@ NSInteger landvariations = 0;
             
         } else if (buttonIndex == 2){
             accessibility = 5;
-            //[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://www.youtube.com"]]];
-            temp_accessibility = 1;
+            [self.accessibilityButton setTitle:@"Accessibility E!" forState:UIControlStateNormal];
+    
         } else if (buttonIndex == 3){
-            accessibility = 3;
-            //[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://www.facebook.com"]]];
-            temp_accessibility = 2;
+            accessibility = 2;
+            [self.accessibilityButton setTitle:@"Accessibility S!" forState:UIControlStateNormal];
+            
         }
         
     } else if (alertView.tag == 9){
@@ -411,12 +333,12 @@ NSInteger landvariations = 0;
             
         } else if (buttonIndex == 2){
             intersections = 5;
-            //[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://www.youtube.com"]]];
-            temp_intersections = 1;
+            [self.intersectionsButton setTitle:@"Intersections E!" forState:UIControlStateNormal];
+            
         } else if (buttonIndex == 3){
-            intersections = 3;
-            //[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://www.facebook.com"]]];
-            temp_intersections = 2;
+            intersections = 2;
+            [self.intersectionsButton setTitle:@"Intersections S!" forState:UIControlStateNormal];
+            
         }
         
     }else if (alertView.tag == 10){
@@ -427,12 +349,12 @@ NSInteger landvariations = 0;
             
         } else if (buttonIndex == 2){
             landvariations = 5;
-            //[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://www.youtube.com"]]];
-            temp_landvariations = 1;
+            [self.landvariationsButton setTitle:@"Land Variations E!" forState:UIControlStateNormal];
+            
         } else if (buttonIndex == 3){
-            landvariations = 3;
-            //[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://www.facebook.com"]]];
-            temp_landvariations = 2;
+            landvariations = 2;
+            [self.landvariationsButton setTitle:@"Land Variations S!" forState:UIControlStateNormal];
+            
         }
         
     }
